a98fed722020925c5a4dbd9b34a6ea48986ccd5f
Samuel <samuelviniciusoficial@gmail.com>
2023-08-21 16:16:06 -0300

    first commit

---
+++ exercicio3.7.py
@@ -0,0 +1,6 @@
+
+num1 = int(input("Digite o primeiro número"))
+num2 = int(input("Digite o segundo número"))
+
+result = num1 + num2
+print(f"A soma dos dois número é {result}")
---
+++ exercicio3.8.py
@@ -0,0 +1,5 @@
+metros = int(input("Digite o valor em metros"))
+
+milimetros = metros * 1000
+
+print(f"Convertido para milimetros fica {milimetros}")
